60d973027c723eb96d10c454bdfd21435931b22c
Cristian Farias <chrisfarias12@hotmail.com>
2021-01-14 05:19:18 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Avergae
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1;
+            Console.WriteLine("Input a number");
+           num1 = Convert.ToInt32 (Console.ReadLine() );
+
+            int num2;
+            Console.WriteLine("Input a number");
+            num2 = Convert.ToInt32 (Console.ReadLine() );
+
+            int num3;
+            Console.WriteLine("Input a number");
+           num3 = Convert.ToInt32 (Console.ReadLine() );
+
+
+            int result = (num1 + num2 + num3) / 3;
+            Console.WriteLine("The average is " + result);
+
+
+
+
+        }
+    }
+}
